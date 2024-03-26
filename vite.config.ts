0600040d34ab96fd88ab2ddf910d6f1a9ef5c6d8
郑素洁 <zhengsujie@52tt.com>
2024-03-26 14:56:07 +0800

    update 支持 less && 自动转 vw && 添加字体 && 添加 flex

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,38 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import path from "path";
+import vue from "@vitejs/plugin-vue";
+import postcssPxToViewport from "postcss-px-to-viewport";
+// import postcssPxToRem from "postcss-px2rem";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  css: {
+    postcss: {
+      plugins: [
+        // postcssPxToRem({
+        //   remUnit: 75, // 基准大小 baseSize
+        // }),
+        postcssPxToViewport({
+          unitToConvert: "px", // 需要转换的单位，默认为"px"；
+          viewportWidth: 375, // 设计稿的视口宽度
+          unitPrecision: 8, // 单位转换后保留的小数位数
+          propList: ["*"], // 要进行转换的属性列表,*表示匹配所有,!表示不转换
+          viewportUnit: "vw", // 转换后的视口单位
+          fontViewportUnit: "vw", // 转换后字体使用的视口单位
+          selectorBlackList: ["html", ".usePx"], // 不进行转换的css选择器，继续使用原有单位
+          minPixelValue: 0, // 设置最小的转换数值
+          mediaQuery: true, // 设置媒体查询里的单位是否需要转换单位
+          replace: true, // 是否直接更换属性值，而不添加备用属性
+          exclude: [/node_modules/], // 忽略某些文件夹下的文件
+        }),
+      ],
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+    extensions: [".ts", ".less"],
+  },
+});
