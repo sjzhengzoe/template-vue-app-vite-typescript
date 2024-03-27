47a37b992ce2adba2022772a3ce968c429aff858
郑素洁 <zhengsujie@52tt.com>
2024-03-27 10:14:30 +0800

    update

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,3 +4,7 @@ export const copyLink = async (content: string) => {
   const { toClipboard } = useClipboard();
   await toClipboard(content);
 };
+
+// 判断当前是手机端还是电脑端
+export const isMobile = () =>
+  document.getElementsByTagName("body")[0].clientWidth < 1180;
